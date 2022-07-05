fc21a039d079867feb298ac5e208113f07fb1021
Jatin-7 <jatinsingh823@gmail.com>
2022-07-05 15:00:41 +0530

    Updates

---
+++ precache-manifest.bd7102d862c1f2834bf01cf9abb293d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b74011ec37fa284c71",
+    "url": "/Jatin-7/jsfirst/static/css/main.7510fd04.chunk.css"
+  },
+  {
+    "revision": "64b74011ec37fa284c71",
+    "url": "/Jatin-7/jsfirst/static/js/main.64b74011.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Jatin-7/jsfirst/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "db30e9cdfb6c01e1e329",
+    "url": "/Jatin-7/jsfirst/static/css/2.c49e2a65.chunk.css"
+  },
+  {
+    "revision": "db30e9cdfb6c01e1e329",
+    "url": "/Jatin-7/jsfirst/static/js/2.db30e9cd.chunk.js"
+  },
+  {
+    "revision": "48b3a3be865f5b71d625",
+    "url": "/Jatin-7/jsfirst/static/js/runtime~main.48b3a3be.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Jatin-7/jsfirst/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4d173e5bf321bcec7b153dd710f2d9cd",
+    "url": "/Jatin-7/jsfirst/index.html"
+  }
+];
